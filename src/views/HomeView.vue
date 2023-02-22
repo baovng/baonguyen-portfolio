b81ba3e2b063756a5b91fbedc25e32d56adcb7b4
dacinve <78516378+dacinve@users.noreply.github.com>
2023-02-22 02:35:41 -0500

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,63 @@
 <template>
-  <div class="logo-container vh-100" v-if="showLogo">
-    <div class="logo-animation">
-      <LogoAnimation />
-    </div>
-  </div>
-  <div class="content-container d-flex flex-column" v-else>
-    <section>
-      <Intro />
-    </section>
-    <section>
-      <About />
-    </section>
-  </div>
+	<div class="logo-container vh-100" v-if="showLogo">
+		<div class="logo-animation">
+			<LogoAnimation />
+		</div>
+	</div>
+	<div class="content-container d-flex flex-column" v-else>
+		<section>
+			<Intro />
+		</section>
+		<section>
+			<About />
+		</section>
+		<section class="mt-5">
+			<Experience />
+		</section>
+	</div>
 </template>
 
 <script>
-import LogoAnimation from "../components/LogoAnimation.vue";
-import About from "@/components/About.vue";
-import Intro from "@/components/Intro.vue";
+	import LogoAnimation from '../components/LogoAnimation.vue';
+	import About from '@/components/About.vue';
+	import Intro from '@/components/Intro.vue';
+	import Experience from '@/components/Experience.vue';
 
-export default {
-  components: {
-    LogoAnimation,
-    About,
-    Intro
-  },
-  data() {
-    return {
-      showLogo: true
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.showLogo = false;
-    }, 3000); // Change 3000 to the duration of your logo animation in milliseconds
-  }
-};
+	export default {
+		components: {
+			LogoAnimation,
+			About,
+			Intro,
+			Experience,
+		},
+		data() {
+			return {
+				showLogo: true,
+			};
+		},
+		mounted() {
+			setTimeout(() => {
+				this.showLogo = false;
+			}, 3000); // Change 3000 to the duration of your logo animation in milliseconds
+		},
+	};
 </script>
 
 <style>
-.logo-container,
-.content-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+	.logo-container,
+	.content-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 
-.logo-container {
-  color: #fff;
-  font-size: 4rem;
-}
+	.logo-container {
+		color: #fff;
+		font-size: 4rem;
+	}
 
-.content-container {
-  font-size: 3rem;
-  color: #fff;
-}
+	.content-container {
+		font-size: 3rem;
+		color: var(--text-color1);
+	}
 </style>
