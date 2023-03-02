fae74ac745ac557590a84a8c4007ab8186787f88
dacinve <78516378+dacinve@users.noreply.github.com>
2023-03-02 14:29:46 -0500

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,117 +1,124 @@
 <template>
-  <div class="container-fluid ps-0 bg-light">
-    <nav class="navbar" :class="navbarClasses" :style="{'transform': translateY}">
-      <div class="container-fluid">
-        <img src="@/assets/logo.png" class="float-start m-3 ms-5" alt="..." style="width: 45px" />
+	<div class="container-fluid ps-0 bg-light">
+		<nav class="navbar" :class="navbarClasses" :style="{ transform: translateY }">
+			<div class="container-fluid mx-5">
+				<a href="/" class="logo"
+					><img src="@/assets/logo1.png" class="float-start m-1 mt-2" alt="..." style="width: 44px"
+				/></a>
 
-        <div
-          class="nav-link nav-link-custom-2 d-flex flex-row justify-content-end align-items-center"
-        >
-          <!-- <router-link to="/teacher/dashboard">Dashboard</router-link> -->
-          <div v-for="(section,index) in sections" :key="index" class="mx-4">
-            <router-link :to="'/' + section.id" class="nav-text-custom">
-              <span>{{ section.id }} .</span>
-              {{ section.section }}
-            </router-link>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </div>
+				<div
+					class="nav-link nav-link-custom-2 d-flex flex-row justify-content-end align-items-center"
+				>
+					<div v-for="(section, index) in sections" :key="index" class="ms-4">
+						<router-link :to="'/' + section.id" class="nav-text-custom">
+							<span>{{ section.id }} .</span>
+							{{ section.section }}
+						</router-link>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      isHidden: false,
-      isFixed: false,
-      prevScrollPosition: window.pageYOffset,
-      sections: [
-        { id: "01", section: "About" },
-        { id: "02", section: "Experiences" },
-        { id: "03", section: "Project" },
-        { id: "04", section: "Contact" }
-      ]
-    };
-  },
-  mounted() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
-  beforeUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  },
-  methods: {
-    handleScroll() {
-      const currentPosition = window.pageYOffset;
+	export default {
+		data() {
+			return {
+				isHidden: false,
+				isFixed: false,
+				prevScrollPosition: window.pageYOffset,
+				sections: [
+					{ id: '01', section: 'About' },
+					{ id: '02', section: 'Experiences' },
+					{ id: '03', section: 'Project' },
+					{ id: '04', section: 'Contact' },
+				],
+			};
+		},
+		mounted() {
+			window.addEventListener('scroll', this.handleScroll);
+		},
+		beforeUnmount() {
+			window.removeEventListener('scroll', this.handleScroll);
+		},
+		methods: {
+			handleScroll() {
+				const currentPosition = window.pageYOffset;
 
-      const scrollDirection =
-        currentPosition > this.prevScrollPosition ? "down" : "up";
+				const scrollDirection = currentPosition > this.prevScrollPosition ? 'down' : 'up';
 
-      if (scrollDirection === "down") {
-        //scroll down, hide navbar after delay
-        this.isHidden = true;
-        this.isFixed = false;
-      } else {
-        //scroll up, show navbar
-        this.isHidden = false;
-        if (currentPosition < window.innerHeight * 0.2) {
-          this.isFixed = false;
-        } else {
-          this.isFixed = true;
-        }
-      }
-      this.prevScrollPosition = currentPosition;
-    }
-  },
-  computed: {
-    navbarClasses() {
-      return {
-        "navbar-hidden": this.isHidden,
-        "box-shadow": this.isFixed && !this.isHidden
-      };
-    },
-    translateY() {
-      return `translateY(${this.isHidden ? "-100%" : 0})`;
-    }
-  }
-};
+				if (scrollDirection === 'down') {
+					//scroll down, hide navbar after delay
+					this.isHidden = true;
+					this.isFixed = false;
+				} else {
+					//scroll up, show navbar
+					this.isHidden = false;
+					if (currentPosition < window.innerHeight * 0.2) {
+						this.isFixed = false;
+					} else {
+						this.isFixed = true;
+					}
+				}
+				this.prevScrollPosition = currentPosition;
+			},
+		},
+		computed: {
+			navbarClasses() {
+				return {
+					'navbar-hidden': this.isHidden,
+					'box-shadow': this.isFixed && !this.isHidden,
+				};
+			},
+			translateY() {
+				return `translateY(${this.isHidden ? '-100%' : 0})`;
+			},
+		},
+	};
 </script>
 
 <style>
-.navbar {
-  width: 100%;
-  background: none;
-  background-color: rgba(30, 30, 30);
-  position: fixed;
-  top: 0;
-  backdrop-filter: blur(8px);
-  border: none;
-  transition: transform 0.38s ease-in-out;
-  z-index: 10;
-  box-shadow: none;
-}
+	.navbar {
+		width: 100%;
+		background: none;
+		background-color: rgba(30, 30, 30);
+		position: fixed;
+		top: 0;
+		backdrop-filter: blur(4px);
+		border: none;
+		transition: transform 0.38s ease-in-out;
+		z-index: 10;
+		box-shadow: none;
+	}
 
-.navbar-hidden {
-  transform: translateY(-100%);
-  box-shadow: none;
-}
-.box-shadow {
-  box-shadow: 0 2px 10px rgba(20, 20, 20, 0.8);
-  background-color: rgba(30, 30, 30, 0.1);
-}
-.nav-link-custom-2 {
-  font-size: 0.95rem;
-  font-family: "Rubik", serif;
-}
-.nav-text-custom {
-  color: var(--text-color1);
-  text-decoration: none;
-}
-.nav-text-custom:hover {
-  color: var(--primary-color);
-}
-.nav-text-custom > span {
-  color: var(--primary-color);
-}
+	.navbar-hidden {
+		transform: translateY(-100%);
+		box-shadow: none;
+	}
+	.box-shadow {
+		box-shadow: 0 2px 10px rgba(20, 20, 20, 0.8);
+		background-color: rgba(30, 30, 30, 0.1);
+	}
+	.nav-link-custom-2 {
+		font-size: 0.95rem;
+		font-family: 'Rubik', serif;
+	}
+	.nav-text-custom {
+		color: var(--text-color1);
+		text-decoration: none;
+	}
+	.nav-text-custom:hover {
+		color: var(--primary-color);
+	}
+	.nav-text-custom > span {
+		color: var(--primary-color);
+	}
+
+	.logo {
+		transition: all 0.3s ease-in-out;
+	}
+	.logo:hover {
+		transform: scale(1.1);
+	}
 </style>
--- src/components/Sm.vue
+++ src/components/Sm.vue
@@ -1,57 +1,57 @@
 <template>
-  <div class="social-icons-container">
-    <div class="social-icons">
-      <a href="#">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="#">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="#">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="#">
-        <i class="fab fa-linkedin-in"></i>
-      </a>
-    </div>
+	<div class="social-icons-container">
+		<div class="social-icons">
+			<a href="https://github.com/dacinve" target="_blank">
+				<i class="fab fa-github"></i>
+			</a>
+			<a href="https://www.linkedin.com/in/bao-nguyen-a08581265/" target="_blank">
+				<i class="fab fa-linkedin-in"></i>
+			</a>
+			<a href="https://www.facebook.com/profile.php?id=100005196251122" target="_blank">
+				<i class="fab fa-facebook-f"></i>
+			</a>
+			<a href="https://www.instagram.com/baovnguyenx/?next=%2F" target="_blank">
+				<i class="fab fa-instagram"></i>
+			</a>
+		</div>
 
-    <div class="horizon-line mt-2"></div>
-  </div>
+		<div class="horizon-line mt-2"></div>
+	</div>
 </template>
 
 <style>
-.social-icons-container {
-  position: fixed;
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-  left: 4%;
-}
+	.social-icons-container {
+		position: fixed;
+		bottom: 0;
+		display: flex;
+		flex-direction: column;
+		left: 4%;
+	}
 
-.horizon-line {
-  width: 1px;
-  height: 100px;
-  background-color: #fff;
-  opacity: 0.5;
-  margin: 0 auto;
-}
+	.horizon-line {
+		width: 1px;
+		height: 100px;
+		background-color: #fff;
+		opacity: 0.5;
+		margin: 0 auto;
+	}
 
-.social-icons {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: -15px;
-}
+	.social-icons {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 
-.social-icons a {
-  margin: 0 15px;
-  color: #fff;
-  font-size: 20px;
-  transition: all 0.2s ease-in-out;
-}
+	.social-icons a {
+		color: #fff;
+		font-size: 20px;
+		margin: 5px 0;
+		transition: all 0.2s ease-in-out;
+	}
 
-.social-icons a:hover {
-  transform: scale(1.1);
-}
+	.social-icons a:hover {
+		transform: scale(1.2);
+		color: var(--primary-color);
+	}
 </style>
